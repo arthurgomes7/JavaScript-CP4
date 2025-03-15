819c025eb9533bb5079d8b735e68294588ebe683
Arthur Gomes <arthurszaa7@icloud.com>
2025-03-15 17:21:30 -0300

    Tentativa de Subir no NPM

---
+++ newConsolelog.js
@@ -0,0 +1,5 @@
+import {randomSupervillain} from 'supervillains';
+
+export function newConsoleLog(mensagem) {
+    console.log("E em um momento de raiva",randomSupervillain(), "disse:", mensagem)
+}
